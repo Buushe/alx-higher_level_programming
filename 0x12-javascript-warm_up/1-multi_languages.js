d38a42c7c18b31c197cd68fe267517ca7005a332
Buushe <aadangoranyo@gmai.com>
2022-11-29 07:55:47 +0000

    done1,2

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myC = "C is a fun\n"
-const myPyhton = "Python is cool\n"
+const myC = "C is a fun\n";
+const myPyhton = "Python is cool\n";
 const myJs = "Javascript is amazing\n";
 console.log(myC + myPyhton +myJs);
