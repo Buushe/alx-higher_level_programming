8fb2cf193aab447e1d6f013e7cd49d5cb4607b7c
Buushe <aadangoranyo@gmai.com>
2022-11-29 08:46:47 +0000

    done js-wrap

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
